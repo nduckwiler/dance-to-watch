8ce4a2b680d7fc8736e2289196f96a4b23c33eab
Nick Duckwiler <nduckwiler@gmail.com>
2020-04-26 17:28:48 -0400

    Update pixel-example for clarity

--- pixel-example.js
+++ pixel-example.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1900dcabd8420919738f09472b891c1e36ecfb07f6e74e3d656e9d62f983bec8
-size 1522
+oid sha256:964fab746646a0f6d2d3247a1e03cc3327d0d912be48ee9a6e91b5f34ef47eb7
+size 1557
