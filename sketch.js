d9d0af986bf5d80dafb9dd00b4e45f7b9a26cbd0
Nick Duckwiler <nduckwiler@gmail.com>
2020-04-26 22:24:47 -0400

    Add p5 boilerplate to sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:845040812cc0cb34973b41b0c265491ee640a9b00713fdc95dd58c2d27b783fd
-size 108
+oid sha256:4e36857e65f07ee504abe88daf48c6ac619f665c3ddeb6e7c28de6e33c67e452
+size 174
