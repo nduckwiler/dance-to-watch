127d143a8d61639fe63851d87ba2aa69c73481ba
Nick Duckwiler <nduckwiler@gmail.com>
2020-06-07 17:30:40 -0400

    Clean up some soundfile code

--- altogether.js
+++ altogether.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7147ce87238aa13e4fd1cffba909ecd122b44a59cad83b00553a85cf2e877ece
-size 4945
+oid sha256:17ac3802e1946e2f70eaaf1db8089857dcabe093897064e453d8f133d81782be
+size 4897
